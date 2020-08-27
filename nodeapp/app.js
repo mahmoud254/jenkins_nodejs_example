136a2a9b77a4a2d96ea503c383bc984462c716cb
Mahmoud Alaa <46284954+mahmoud254@users.noreply.github.com>
2020-08-27 21:10:11 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello from dev!')
   })
   
   app.listen(port, () => {
