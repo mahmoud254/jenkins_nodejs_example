0759ba2320765a725fd6a7a34c1305ee30283c97
Mahmoud Alaa <46284954+mahmoud254@users.noreply.github.com>
2021-04-04 23:24:16 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,4 +1,34 @@
 const express = require('express')
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
+
+const redis = require('redis');
+const client = redis.createClient({
+    host: process.env.REDIS_HOSTNAME,
+    port: REDIS_PORT,
+});
+
+client.on('error', err => {
+    console.log('Error ' + err);
+});
+
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
