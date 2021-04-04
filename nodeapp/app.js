41a97cda1bb871a1c09f4b79d0488ef1be822f96
Mahmoud Alaa <46284954+mahmoud254@users.noreply.github.com>
2021-04-04 19:13:07 -0400

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 var mysql = require('mysql');
+const app = express()
+const port = 3000
 
 var connection = mysql.createConnection({
   host     : process.env.RDS_HOSTNAME,
@@ -7,17 +9,19 @@ var connection = mysql.createConnection({
   password : process.env.RDS_PASSWORD,
   port     : process.env.RDS_PORT
 });
+app.get("/db", (req, res) => {
 
 connection.connect(function(err) {
   if (err) {
+	  res.send("db connection failed")
     console.error('Database connection failed: ' + err.stack);
     return;
-  }
-
+ }
+	res.send("db connection successful");
   console.log('Connected to database.');
-});
 
 connection.end();
+});})
 
 const redis = require('redis');
 const client = redis.createClient({
@@ -29,10 +33,18 @@ client.on('error', err => {
     console.log('Error ' + err);
 });
 
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
-    res.send('Hello from dev!')
+app.get('/redis', (req, res) => {
+
+  client.set('foo','bar', (error, rep)=> {                
+    if(error){     
+console.log(error);
+      res.send("redis connection failed");                             
+      return;                
+  }                 
+  if(rep){                          //JSON objects need to be parsed after reading from redis, since it is stringified before being stored into cache                      
+ console.log(rep);
+  res.send("redis is successfuly connected");                 
+ }}) 
   })
   
   app.listen(port, () => {
