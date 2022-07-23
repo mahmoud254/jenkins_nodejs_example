d0d205bab8d03edd49ff15cdee5d91ce65ceb252
Mahmoud Alaa <46284954+mahmoud254@users.noreply.github.com>
2022-07-23 10:07:35 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from ITI!')
+    res.send('Hello World from ITI 3 month')
   })
   
   app.listen(port, () => {
